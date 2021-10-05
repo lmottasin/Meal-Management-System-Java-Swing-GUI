bd78169d93215752dc805e19ee723f4b0385d821
M Lemon <lmottasin@gmail.com>
2021-10-05 21:30:14 +0600

    meal mng. system

---
+++ src/mealmanagementsystem/MealManagementSystem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mealmanagementsystem;
+
+/**
+ *
+ * @author Leemon
+ */
+public class MealManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     LoginPage n= new LoginPage();
+     n.setVisible(true);
+    }
+    
+}
